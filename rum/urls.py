61f583dc3285f14d5b4babf482081183ce267d94
Sebastian <sebaajara@hotmail.com>
2024-05-29 22:28:11 -0400

    Se migra a Django la mayoria de cosas, falta migrar imagenes y hacer funcionar login.html

--- rum/urls.py
+++ rum/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("",views.index,name='index'),
+    path("servicio",views.servicio,name='servicio'),
+    path("informacion", views.informacion, name="informacion"),
+    path("contacto",views.contacto,name='contacto'),
+    path("clientes",views.clientes,name='clientes'),
+    path("login",views.login,name='login'),
 ]
--- rum/views.py
+++ rum/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 def index(request):
     context ={}
     return render(request,'pages/index.html',context)
+def servicio(request):
+    context ={}
+    return render(request, "pages/servicio.html",context)
+def informacion(request):
+    context ={}
+    return render(request, "pages/informacion.html",context)
+def contacto(request):
+    context ={}
+    return render(request, "pages/contacto.html",context)
+def clientes(request):
+    context ={}
+    return render(request, "pages/clientes.html",context)
+def login(request):
+    context ={}
+    return render(request, "pages/login.html",context)
