2a331b77423b40a18ef5dc94a8cb31507499ba76
Sebastian <sebaajara@hotmail.com>
2024-06-13 22:26:35 -0400

    Se agrega cosas a registro, se crean mas tablas para usuario

--- rum/models.py
+++ rum/models.py
@@ -7,10 +7,12 @@ class Tier(models.Model):
     tier = models.CharField(max_length=1, blank=False, null=False)
 
 class Usuario(models.Model):
+    id_usuario = models.AutoField(db_column='idUsuario', primary_key=True)
     nombre = models.CharField(max_length=20)
+    apellido_paterno = models.CharField(max_length=20)
+    apellido_materno = models.CharField(max_length=20)
     email = models.EmailField(unique=True, max_length=100, blank="false", null="true")
     password = models.CharField(max_length=30, blank="true", null="true")
     id_tier = models.ForeignKey(
         "Tier", on_delete=models.CASCADE, db_column="IDTier"
     )
-    activo = models.IntegerField()
